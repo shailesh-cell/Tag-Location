4af71f06d90daf5fbd2690199cf50d6246909f0c
sc1 <s.c1@mail.com>
2025-02-21 11:34:22 +0000

    Added tenant id in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,8 @@
 
 terraform {
   backend "azurerm" {
+    tenant_id           = "21150d7d-bb5a-486f-86af-73c82a8240b"
+    subscription_id     = "a6435a3c-312e-41ed-878b-3118525f919a"
     resource_group_name   = "terraform_tfstate_rg"
     storage_account_name  = "terraformconfigsa"
     container_name        = "tfstate"
