1e1acd3de569e36d45f3a2bafcff72c73ac3026d
sc1 <s.c1@mail.com>
2025-02-21 19:52:43 +0530

    main.tf tag name

--- main.tf
+++ main.tf
@@ -12,4 +12,10 @@ resource "azurerm_resource_group_policy_assignment" "tag_policy_assignment" {
   description          = "Ensures that all resource groups have a CostCenter tag."
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/96670d01-0a4d-4649-9c89-2d3abc0a5025"
   resource_group_id    = azurerm_resource_group.rg.id
+
+  parameters = jsonencode({
+    tagName = {
+      value = "CostCenter"  # Replace "CostCenter" with the actual tag name you want to enforce
+    }
+  })
 }
