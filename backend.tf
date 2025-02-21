a39f05e5412074223b73ff64d4c81f87d03d283a
sc1 <s.c1@mail.com>
2025-02-21 11:36:22 +0000

    Added client id in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,9 @@
 
 terraform {
   backend "azurerm" {
-    tenant_id           = "21150d7d-bb5a-486f-86af-73c82a8240b"
-    subscription_id     = "a6435a3c-312e-41ed-878b-3118525f919a"
+    tenant_id             = "21150d7d-bb5a-486f-86af-73c82a8240b"
+    subscription_id       = "a6435a3c-312e-41ed-878b-3118525f919a"
+    Client_id             = "72533fa1-c58b-4071-8df5-eb25d55b1568"
     resource_group_name   = "terraform_tfstate_rg"
     storage_account_name  = "terraformconfigsa"
     container_name        = "tfstate"
