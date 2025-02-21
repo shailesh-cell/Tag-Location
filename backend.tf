96dced5a9f50ef2f5f5aeb6532943dbb597d7f48
sc1 <s.c1@mail.com>
2025-02-21 09:57:59 +0000

    Policy

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# backend.tf
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terraform_tfstate_rg"
+    storage_account_name  = "terraformconfigsa"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+# variables.tf
+
+variable "subscription_id" {
+  description = "The subscription ID for Azure"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for the Service Principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "The client secret for the Service Principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for Azure"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the resources"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources"
+  type        = map(string)
+}
