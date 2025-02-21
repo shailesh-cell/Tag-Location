4520bab5120feaed37fbe48feedcecea0b12014e
sc1 <s.c1@mail.com>
2025-02-21 11:42:08 +0000

    removed subscription id in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,6 @@
 
 terraform {
   backend "azurerm" {
-    tenant_id             = "21150d7d-bb5a-486f-86af-73c82a8240b"
-    Client_id             = "72533fa1-c58b-4071-8df5-eb25d55b1568"
     resource_group_name   = "terraform_tfstate_rg"
     storage_account_name  = "terraformconfigsa"
     container_name        = "tfstate"
