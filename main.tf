adccc6f387c5297e511aa913b1a2bf1b1d2849c9
sc1 <s.c1@mail.com>
2025-02-21 17:53:18 +0530

    resource tag policy built in

--- main.tf
+++ main.tf
@@ -6,28 +6,10 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
-resource "azurerm_resource_policy_definition" "tag_policy" {
-  name         = "require-tags"
-  policy_type  = "Custom"
-  mode         = "All"
-  display_name = "Require specific tags"
-  description  = "Ensure specific tags are present on resource groups."
-
-  policy_rule = jsonencode({
-    if: {
-      allOf: [
-        { field: "type", equals: "Microsoft.Resources/subscriptions/resourceGroups" },
-        { field: "tags.CostCenter", exists: false }
-      ]
-    },
-    then: {
-      effect: "deny"
-    }
-  })
-}
-
-resource "azurerm_resource_policy_assignment" "tag_policy_assignment" {
-  name                 = "tag_policy_assignment"
-  policy_definition_id = azurerm_policy_definition.tag_policy.id
-  scope                = azurerm_resource_group.rg.id
+resource "azurerm_resource_group_policy_assignment" "tag_policy_assignment" {
+  name                 = "tag-policy-assignment"
+  display_name         = "Require CostCenter Tag"
+  description          = "Ensures that all resource groups have a CostCenter tag."
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/96670d01-0a4d-4649-9c89-2d3abc0a5025"
+  resource_group_id    = azurerm_resource_group.rg.id
 }
