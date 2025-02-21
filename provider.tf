c16cf894994006f4cc18e1584c777919ab6467e8
sc1 <s.c1@mail.com>
2025-02-21 19:37:07 +0530

    yml_9

--- provider.tf
+++ provider.tf
@@ -1,6 +1,18 @@
 # provider.tf
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 
+  tenant_id       = "21150d7d-bb5a-486f-86af-73c82a8240bc"
+
 }
+
