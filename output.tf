f9f775a5d66859fd586510b69f743b12a7f0fdec
sc1 <s.c1@mail.com>
2025-02-22 19:01:12 +0530

    policy&location

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
 # output.tf
 
-output "location_policy_name" {
+output "policy_name" {
   value       = azurerm_policy_definition.location_policy.name
   description = "The name of the created Azure Policy definition."
 }
 
-output "location_policy_assignment_id" {
-  value       = azurerm_subscription_policy_assignment.location_policy.id
+output "policy_assignment_id" {
+  value       = azurerm_subscription_policy_assignment.location_policy_assignment.id
   description = "The ID of the assigned Azure Policy to enforce tagging and location restrictions."
 }
 
-output "tagging_policy_name" {
+output "policy_name" {
   value       = azurerm_policy_definition.tagging_policy.name
   description = "The name of the created Azure Policy definition."
 }
 
-output "tagging_policy_assignment_id" {
-  value       = azurerm_subscription_policy_assignment.tagging_policy.id
+output "policy_assignment_id" {
+  value       = azurerm_subscription_policy_assignment.tagging_policy_assignment.id
   description = "The ID of the assigned Azure Policy to enforce tagging and location restrictions."
 }
 
