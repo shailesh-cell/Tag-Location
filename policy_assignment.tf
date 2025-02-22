3f3c5bc25e8bc31c2408267e03b6b6fe0b933c49
sc1 <s.c1@mail.com>
2025-02-22 19:24:45 +0530

    policy&location

--- policy_assignment.tf
+++ policy_assignment.tf
@@ -1,7 +1,33 @@
 # policy_assignment.tf
 
+resource "azurerm_subscription_policy_assignment" "tagging_policy_assignment" {
+  name                 = "tagging-policy-assignment"
+  scope                = data.azurerm_subscription.current.id  # Corrected scope
+  policy_definition_id = azurerm_policy_definition.tagging_policy.id
+
+  parameters = <<PARAMETERS
+{
+  "requiredTags": {
+    "value": {
+      "tag1": "CostCenter",
+      "tag2": "Environment",
+      "tag3": "Owner"
+    }
+  }
+}
+PARAMETERS
+}
+
 resource "azurerm_subscription_policy_assignment" "location_policy_assignment" {
   name                 = "location-policy-assignment"
-  scope                = data.azurerm_subscription.current.id
+  scope                = data.azurerm_subscription.current.id  # Corrected scope
   policy_definition_id = azurerm_policy_definition.location_policy.id
+
+  parameters = <<PARAMETERS
+{
+  "allowedLocations": {
+    "value": ["centralindia", "southindia", "westindia"]
+  }
+}
+PARAMETERS
 }
