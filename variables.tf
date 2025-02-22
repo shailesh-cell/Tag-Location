b9eec90f28a288c1e1dc3041afa3dc1ebaa745d9
sc1 <s.c1@mail.com>
2025-02-22 20:37:13 +0530

    policy&location

--- variables.tf
+++ variables.tf
@@ -9,8 +9,3 @@ variable "required_tags" {
   description = "Tags that must be applied to all resources"
   type        = list(string)  # Changed from map(string) to list(string) for consistency
 }
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
