0d005cffe52f3c2400330949ed57053a8c269adf
sc1 <s.c1@mail.com>
2025-02-23 16:15:31 +0530

    management group

--- managment_group.tf
+++ managment_group.tf
@@ -1,5 +1,5 @@
 
 # Reference the existing Management Group
-data "azurerm_management_group" "Development_mg" {
+data "azurerm_management_group" "Development-mg" {
   name = "Development-mg"  # Name of the existing Management Group
 }
