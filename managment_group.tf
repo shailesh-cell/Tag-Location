c974f920f62d7770886978b6fc6baed88d20ef9e
sc1 <s.c1@mail.com>
2025-02-23 16:02:31 +0530

    management group

--- managment_group.tf
+++ managment_group.tf
@@ -1,3 +1,5 @@
-resource "azurerm_management_group" "Development-mg" { 
-  display_name = "Development Management Group"
+
+# Reference the existing Management Group
+data "azurerm_management_group" "Development_mg" {
+  name = "Development-mg"  # Name of the existing Management Group
 }
